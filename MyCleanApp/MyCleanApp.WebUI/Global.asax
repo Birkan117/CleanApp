8469f85c1ecc3406260e693f692b2b245a7e6306
Birkan117 <57661682+Birkan117@users.noreply.github.com>
2020-06-10 13:25:28 +0100

    Initial SetUp Solution

---
+++ MyCleanApp/MyCleanApp.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyCleanApp.WebUI.MvcApplication" Language="C#" %>
---
+++ MyCleanApp/MyCleanApp.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyCleanApp.WebUI.Startup))]
+namespace MyCleanApp.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
