92df3a7733f9551d2744d87daafef85c73fbb76e
Birkan117 <57661682+Birkan117@users.noreply.github.com>
2020-06-12 09:49:23 +0100

    Created the SQL database using entity framework

---
+++ MyCleanApp/MyCleanApp.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using MyCleanApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCleanApp.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        //First create constructor to capture and pass through the connection string the base class is expecting
+        public DataContext(): base("DefaultConnection")
+        {
+
+        }
+        //Telling the datacontext what models we want stored into the database.
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
