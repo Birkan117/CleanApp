4c36e611e0150f684a5819f8ffec59e78c68be4e
Birkan117 <57661682+Birkan117@users.noreply.github.com>
2020-06-14 21:26:27 +0100

    Product Listing on the home page has been created.

--- MyCleanApp/MyCleanApp.WebUI/Controllers/HomeController.cs
+++ MyCleanApp/MyCleanApp.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyCleanApp.Core.Contracts;
+using MyCleanApp.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,43 @@ namespace MyCleanApp.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        //Need To first create an instance of the product repository
+        //ProductRepository context;
+        //ProductCategoryRepository productCategories;
+
+
+        //InMemoryRepository<Product> context;
+        //InMemoryRepository<ProductCategory> productCategories;
+
+        //The Code will now use IRepository instead (Due to dependancy injection)
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+
+        //Then create a constructor of the productManagerController that initialises the product repository
+        //Injecting the classes through the constructor bellow. "IRepository<>"
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            productCategories = productCategoryContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
